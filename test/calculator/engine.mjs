8e6eb393d132db08008179932de6d9943697a51b
fengbaiyang <fengbaiyang@didichuxing.com>
2021-03-17 12:55:14 +0800

    calc-v3

--- test/calculator/engine.mjs
+++ test/calculator/engine.mjs
@@ -3,7 +3,7 @@ import Token from './token'
 class Engine {
   constructor(options) {
     this._tokenizer = new Token({
-      rule: options.tokenRule // fixme 
+      rule: options.tokenRule // fixme 空值需要处理一下
     })
     this._parser = new Parse({
       parseRule: options.parseRule // fixme 不传的情况处理
